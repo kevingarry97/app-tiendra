f84045a2afb7e18faa2da46bb639d2649ae8a6df
kevingarry97 <kevingarry97@gmail.com>
2020-07-20 13:21:38 -0700

    Modified

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
 import { MailService } from '../shared/mail.service';
 import { OrdersService } from '../shared/orders.service';
 import { ProductService } from '../shared/product.service';
@@ -13,10 +14,13 @@ export class DashboardComponent implements OnInit {
   discount = [];
   mails = [];
   orders = [];
+  category = [];
+  sub = [];
   constructor(
     private mailService: MailService,
     private productService: ProductService,
-    private orderService: OrdersService
+    private orderService: OrdersService,
+    private categories: CategoryService,
   ) { }
 
   ngOnInit(): void {
@@ -26,7 +30,7 @@ export class DashboardComponent implements OnInit {
       })
 
     this.productService.getProduct()
-    .subscribe((data: any) => {
+      .subscribe((data: any) => {
       this.product = data;
     })
 
@@ -36,9 +40,19 @@ export class DashboardComponent implements OnInit {
       })
 
     this.productService.getDiscount()
-    .subscribe((data: any) => {
+      .subscribe((data: any) => {
       this.discount = data;
     })
+
+    this.categories.getCategory()
+      .subscribe((data: any) => {
+        this.category = data;
+      })
+
+    this.categories.getSubs()
+      .subscribe((data: any) => {
+        this.sub = data;
+      })
   }
 
 }
